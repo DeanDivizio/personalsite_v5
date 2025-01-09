502a90870234d1be1fdae6d9d2bc5fa1ae5b09e0
Dean Divizio <dean@deandivizio.com>
2025-01-09 11:27:15 -0500

    added google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import Background from "../components/Background";
 import Navigation from "../components/Navigation";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 
 const inter = Inter({
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}> 
+      <GoogleAnalytics gaId="G-B4LMP9JX7Y" />
         <Navigation />
         <main className="w-[100vw] min-h-[100vh]">
           {children}
