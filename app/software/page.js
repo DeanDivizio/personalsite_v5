8f8b355041d6f47616f6a41cb90cc62165243816
Dean Divizio <dean@deandivizio.com>
2025-03-20 20:40:26 -0400

    removed book tracker since i'm not maintaining it anymore

--- app/software/page.js
+++ app/software/page.js
@@ -16,16 +16,8 @@ export default function SoftwarePage() {
                         <p className="text-sm 2xl:text-lg mb-4 font-extralight tracking-wider">{`I wanted a single piece of software for tracking all aspects of my health and fitness; complete with great graphs and a modern UI. I couldn't find one I liked, so I decided to make one.`}</p>
                         <p className="text-sm 2xl:text-lg mb-8 font-extralight tracking-wider">{`Apexion is still in active development but `}you can <a className="underline text-green-400 hover:text-blue-500 transition-colors duration-300" target="_blank" href={`https://github.com/DeanDivizio/apexion-health`}>check out the code here</a>{` if you're interested.`}</p>
                         <h3 className="text-xl 2xl:text-2xl mb-4 font-regular">{`What I've Built`}</h3>
-                        <p className="text-sm 2xl:text-lg mb-8 font-extralight tracking-wider">I've built a variety of projects, from marketing websites to headless e-commerce and Apexion. Most are <a className="underline text-green-400 hover:text-blue-500 transition-colors duration-300" target="_blank" href="https://github.com/DeanDivizio">available on my GitHub</a> but here are a few of my favorites.</p>
+                        <p className="text-sm 2xl:text-lg mb-8 font-extralight tracking-wider">I've built a variety of projects, from marketing websites to headless e-commerce and Apexion. Most are <a className="underline text-green-400 hover:text-blue-500 transition-colors duration-300" target="_blank" href="https://github.com/DeanDivizio">available on my GitHub</a> but this is one of of my favorites.</p>
                         <div className="grid grid-cols-1 4xl:grid-cols-2 justify-start gap-8 flex-wrap mb-8 4xl:px-2">
-                        <Link href={"https://booktracker.deandivizio.com"} className="hover:scale-[102%] transition-transform duration-500" target="_blank">
-                                <Card className="">
-                                    <CardHeader>
-                                        <CardTitle>Book Tracker</CardTitle>
-                                        <CardDescription>{`I wanted a streamlined/utilitarian way for keeping track of the books I want/have/read, so I built one. `}<span className="italic">{`Clicking this will take you to a read-only instance for demo purposes.`}</span></CardDescription>
-                                    </CardHeader>
-                                </Card>
-                            </Link>
                             <Link href={"https://www.ovrtonemedia.com"} className="hover:scale-[102%] transition-transform duration-500" target="_blank">
                                 <Card className="">
                                     <CardHeader>
@@ -34,7 +26,6 @@ export default function SoftwarePage() {
                                     </CardHeader>
                                 </Card>
                             </Link>
-                           
                         </div>
                         <h3 className="lg:text-xl 2xl:text-2xl mb-4 font-regular">{`What I Use`}</h3>
                         <p className="text-sm 2xl:text-lg mb-4 font-extralight tracking-wider">{`I absolutely love learning new things. To that end, here are some of the tools I've familiarized myself with over the years.`}</p>
